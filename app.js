4e89b5f6289960dcdc8127775db4a2319c428182
Shashi Kumar M <shashikumarm052@gmail.com>
2021-10-07 16:18:11 +0530

    Add hamburger event listeners and styles

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 
 const hamburger = document.querySelector('.hamburger');
+const header = document.querySelector('.main-header');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('clicked');
+    header.classList.toggle('header-click');
+    hamburgerMenu.classList.toggle('menu-clicked');
 });
